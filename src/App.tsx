38c5072c3a108c968fd7ecd04d3a82a530ead6d6
Arthur D'Afonseca <arthur.dafonseca89@gmail.com>
2020-06-15 19:12:06 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import SignIn from './pages/SignIn';
+// import SignUp from './pages/SignUp';
+import GlobalStyle from './styles/global';
+
+import AppProvider from './hooks/index';
+
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <Router>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
+
+    <GlobalStyle />
+  </Router>
+);
+
+export default App;
