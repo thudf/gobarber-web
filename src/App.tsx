fb65d3bfb66d230875a92f55e8fffd06032c6ccf
Arthur <arthur.dafonseca89@gmail.com>
2020-07-13 22:00:33 -0300

    Final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 import AppProvider from './hooks/index';
